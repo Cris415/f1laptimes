17cc610f62ce4a77737fc23e52e1d82ef974227b
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-04-26 22:13:34 -0700

    add d3 to project

--- entry.js
+++ entry.js
@@ -1,8 +1,9 @@
-document.addEventListener('onContentLoaded', () => {
+import * as d3 from "d3";
+document.addEventListener("DOMContentLoaded", () => {
   const pTag = document.createElement("p");
   const text = document.createTextNode("Welcome!");
   pTag.appendChild(text);
   const element = document.getElementById("root");
-  console.log('testing')
   element.appendChild(pTag);
-})
+
+});
